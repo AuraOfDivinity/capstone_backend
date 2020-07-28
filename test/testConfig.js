6960dabd11fe7999f8bc097ce0c55a28144edad0
Asel <peirisasel373@gmail.com>
2020-07-28 09:57:06 +0530

    minor changes

--- test/testConfig.js
+++ test/testConfig.js
@@ -1,6 +1,8 @@
 //During the automated test the env variable, We will set it to "test"
 process.env.NODE_ENV = "test";
-// process.env.MONGODB_URL = "mongodb://127.0.0.1:27017/rest-api-nodejs-mongodb-test";
+
+// Change to DB instance before testing
+process.env.MONGODB_URL = "mongodb://127.0.0.1:27017/rest-api-nodejs-mongodb-test";
 
 //Require the dev-dependencies
 let chai = require("chai");
